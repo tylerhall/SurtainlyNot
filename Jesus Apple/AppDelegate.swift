0cee2d919ed13151ae2271f943bba2cfe1b0b719
Tyler Hall <rth@tyler.io>
2020-09-05 09:16:00 -0500

    Initial Commit

---
+++ Jesus Apple/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Jesus Apple
+//
+//  Created by Tyler Hall on 9/5/20.
+//  Copyright © 2020 Tyler Hall. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Jesus Apple/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Jesus Apple
+//
+//  Created by Tyler Hall on 9/5/20.
+//  Copyright © 2020 Tyler Hall. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
